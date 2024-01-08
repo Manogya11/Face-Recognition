3cb027958402904f247535ae64f155a7163e68ca
Manogya11 <138658085+Manogya11@users.noreply.github.com>
2024-01-09 00:08:08 +0530

    EncodeGenerator.py

--- EncodeGenerator.py
+++ EncodeGenerator.py
@@ -9,8 +9,8 @@ from firebase_admin import storage
 
 cred = credentials.Certificate("serviceAccountKey.json")
 firebase_admin.initialize_app(cred, {
-    'databaseURL': "https://face-recognition-94653-default-rtdb.firebaseio.com/",
-    'storageBucket': "face-recognition-94653.appspot.com"
+    'databaseURL': "",
+    'storageBucket': ""
 })
 
 
